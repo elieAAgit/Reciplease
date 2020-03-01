c2764cf02d1035f75f66e91f95d937e90673947d
Elie <milhouze@live.fr>
2020-03-01 19:52:11 +0100

    - Creating Search scene in View. _ creating empty ViewController in Controller: SearchViewController.

---
+++ Reciplease/Controller/SearchViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SearchViewController.swift
+//  Reciplease
+//
+//  Created by Elie Arquier on 01/03/2020.
+//  Copyright © 2020 Elie Arquier. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
