024587e16581c8285113051f6425a80cbba507b9
Elie <milhouze@live.fr>
2020-03-02 22:12:50 +0100

    - Adding TableView in Favorite scene and creating FavoriteViewController. - Adding TableView in Search TableView scene and creating SearchTableViewController.
    - Add ScrollView in Search Scene.

---
+++ Reciplease/Controller/SearchTableViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SearchTableViewController.swift
+//  Reciplease
+//
+//  Created by Elie Arquier on 02/03/2020.
+//  Copyright © 2020 Elie Arquier. All rights reserved.
+//
+
+import UIKit
+
+class SearchTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
